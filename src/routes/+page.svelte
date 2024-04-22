9b4179605207aeb6d482080777fe98da6f3cc2a8
Petras Surna <petras.surna@yart.com.au>
2024-04-23 00:02:24 +1000

    first

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.href = '/src/css/main.min.css';
+    document.head.appendChild(link);
+  });
+
+
+</script>
+
+<nav>
+  <a href="/">Ingredients</a>
+  <a href="/recipes">Recipes</a>
+</nav>
+
+<h1>Home</h1>
+<p>This is the home page1.</p>
+
+
